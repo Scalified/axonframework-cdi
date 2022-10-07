88f43c9040c1df3ec87ae4f85471fe863d49bbd4
Vladyslav Baidak <vld.x@hotmail.com>
2022-10-07 11:53:22 +0300

    # 4.6.1-RC1

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ allprojects {
 	val lombokVersion by extra("1.18.24")
 	val javaeeVersion by extra("8.0.1")
 	val commonsLang3Version by extra("3.11")
-	val axonVersion by extra("4.6.0")
+	val axonVersion by extra("4.6.1")
 
 	group = "com.scalified"
 	version = "$axonVersion-RC1"
